cd547a33b2f839e30745ae589eb625433655c313
ZIHAD22 <mdzihad2244@gmail.com>
2024-07-30 01:21:45 +0600

    design header

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,60 +1,36 @@
 import React from "react";
-import { Layout, Menu, theme } from "antd";
-import { Outlet } from "react-router-dom";
-import logo from "./../../assets/icons/logo.webp";
-
-const { Header, Content, Footer } = Layout;
-
-const items = new Array(2).fill(null).map((_, index) => ({
-  key: index + 1,
-  label: `nav ${index + 1}`,
-}));
+// import { Link } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
 
 const MainLayout: React.FC = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   return (
-    <Layout>
-      <Header
-        style={{
-          position: "sticky",
-          top: 0,
-          zIndex: 1,
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <div className="demo-logo">
-          <img src={logo} alt="" />
-        </div>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={items}
-          style={{ flex: 1, minWidth: 0 }}
-        />
-      </Header>
-      <Content style={{ padding: "0 48px" }}>
-        <div
-          style={{
-            padding: 24,
-            minHeight: 380,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <h1>content</h1>
-          <Outlet />
-        </div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout>
+    <div className="mx-auto container  ">
+      <div className="flex  items-center justify-between py-3 ">
+        <a href="" className="flex items-center">
+          <h3 className="font-extrabold bg-green-600 p-2 rounded-md tracking-widest text-xl">
+            E-Nursery
+          </h3>
+        </a>
+        <NavigationMenu className="">
+          <NavigationMenuList>
+            <div className="flex justify-end">
+              <div>
+                <NavigationMenuItem>
+                  <NavigationMenuLink className=" hover:text-green-600 hover:bg-[#bfedd0] delay-100 duration-300">
+                    Movies
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              </div>
+            </div>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
   );
 };
 
