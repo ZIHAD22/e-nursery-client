7ae1ddc7eb5d01914d5600b03c24b3632303444c
ZIHAD22 <mdzihad2244@gmail.com>
2024-08-13 00:54:20 +0600

    cart icon add on navagation

--- src/pages/Shared/Navbar.tsx
+++ src/pages/Shared/Navbar.tsx
@@ -1,9 +1,8 @@
-// import { Link } from "react-router-dom";
 import { Input } from "@/components/ui/input";
+import { FaCartShopping } from "react-icons/fa6";
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
 } from "@radix-ui/react-navigation-menu";
 import logo from "../../assets/icons/logo.png";
@@ -25,18 +24,31 @@ const Navbar: React.FC = () => {
           <NavigationMenuList>
             <div className="flex justify-end">
               <NavigationMenuItem>
-                <NavigationMenuLink className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1">
-                  <Link to="/products">Products</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1">
+                <Link
+                  to="/products"
+                  className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1"
+                >
                   Products
-                </NavigationMenuLink>
-                <NavigationMenuLink className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1">
+                </Link>
+                <Link
+                  to="/"
+                  className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1"
+                >
                   Products
-                </NavigationMenuLink>
-                <NavigationMenuLink className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1">
+                </Link>
+                <Link
+                  to="/"
+                  className="p-3 rounded-md font-medium hover:text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer hover:border-b-[1px] hover:border-b-[var(--bGreen)] mr-1"
+                >
                   Products
-                </NavigationMenuLink>
+                </Link>
+                <Link
+                  to="/"
+                  className="p-3 rounded-md font-medium text-[var(--bGreen)] hover:bg-[var(--wGreen)] delay-75 duration-200 cursor-pointer border border-[var(--bGreen)] mr-1"
+                >
+                  <FaCartShopping size="20px" className="inline-block" />
+                  <span className="text-[20px] pl-2">0</span>
+                </Link>
               </NavigationMenuItem>
             </div>
           </NavigationMenuList>
