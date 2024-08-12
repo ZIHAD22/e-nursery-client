066f58655b1bd8f24df709aa5fbb81a6ca9c5007
ZIHAD22 <mdzihad2244@gmail.com>
2024-08-12 20:28:22 +0600

    add add to cart button

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import AddToCard from "./AddToCard";
+import AddToCardButton from "./components/AddToCardButton";
 
 const ProductDetails = () => {
   return (
@@ -11,9 +12,12 @@ const ProductDetails = () => {
             className="shadow-2xl rounded-xl"
           />
         </div>
-        <div className="bg-red">
+        <div className="">
           <AddToCard />
         </div>
+      </div>
+      <div className="mx-auto w-[600px] mt-5">
+        <AddToCardButton />
       </div>
     </div>
   );
