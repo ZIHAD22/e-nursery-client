c4ae7253852020cf54ef25adc063858ee95f758f
ZIHAD22 <mdzihad2244@gmail.com>
2024-08-16 00:57:09 +0600

    add warrentry card

---
+++ src/pages/Shared/WarrantyCard.tsx
@@ -0,0 +1,17 @@
+import warrantyImg from "../../assets/icons/warranty-img.svg"
+
+const WarrantyCard = () => {
+  return (
+    <div>
+      <div>
+        <img src={warrantyImg} alt="" />
+      </div>
+      <div>
+      1 YEAR WARRANTY SECURED
+      3 plants guaranteed
+      </div>
+    </div>
+  );
+};
+
+export default WarrantyCard;
